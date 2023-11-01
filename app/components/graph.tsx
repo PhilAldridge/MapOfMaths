0f00ab2e59bec42adb5c23691c4bff65035fcb1d
phil <phil.aldridge@bjss.com>
2023-11-01 15:21:03 +0000

    Moved graph to dynamic component

---
+++ app/components/graph.tsx
@@ -0,0 +1,46 @@
+'use client'
+import NeoVis, { NeoVisEvents } from 'neovis.js'
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+
+export default function Graph() {
+  
+  const router = useRouter();
+  useEffect(()=>{
+    const config = {
+      containerId: "viz",
+      dataFunction: getGraphData,
+      labels: {
+        Atom: {
+          label: 'Name',
+          group: 'TypeId'
+        }
+      },
+      visConfig:{
+        nodes:{
+          font: "14px verdana black",
+        },
+      },
+      
+    }
+
+    let neoViz = new NeoVis(config);
+    neoViz.render();
+
+    neoViz.registerOnEvent("completed" as NeoVisEvents, (e:any):void=>{
+      neoViz.network?.on("selectNode", async (event) => {
+        const internalId= event.nodes[0];
+        const result = await fetch(`/api/getId/${internalId}`);
+        const id = await result.json();
+        router.push(`/admin/${id}`)
+      })
+    })
+  },[router])
+  return (
+      <div id="viz" className=' w-screen h-screen p-4 text-lg'></div>
+  )
+}
+
+async function getGraphData() {
+  return (await fetch(`/api`)).json();
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,12 @@
 'use client'
-import NeoVis, { NeoVisEvents } from 'neovis.js'
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
+import dynamic from 'next/dynamic'
 
-type GraphNode = {
-  id: string;
-  x: number;
-  y: number; 
-  label: string
-}
+const Graph = dynamic(()=> import("@/app/components/graph"),{ssr:false})
 
 export default function Home() {
-  const router = useRouter();
-  useEffect(()=>{
-    const config = {
-      containerId: "viz",
-      dataFunction: getGraphData,
-      labels: {
-        Atom: {
-          label: 'Name',
-          group: 'TypeId'
-        }
-      },
-      visConfig:{
-        nodes:{
-          font: "14px verdana black",
-        },
-      },
-      
-    }
-
-    let neoViz = new NeoVis(config);
-    neoViz.render();
-
-    neoViz.registerOnEvent("completed" as NeoVisEvents, (e:any):void=>{
-      neoViz.network?.on("selectNode", async (event) => {
-        const internalId= event.nodes[0];
-        const result = await fetch(`/api/getId/${internalId}`);
-        const id = await result.json();
-        router.push(`/admin/${id}`)
-      })
-    })
-  },[router])
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-100">
-      <div id="viz" className=' w-screen h-screen p-4 text-lg'></div>
+      <Graph />
     </main>
   )
-}
-
-async function getGraphData() {
-  return (await fetch(`/api`)).json();
 }
