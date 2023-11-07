5b74bb05e84b42adc579a5b7276a7eb0687cde45
phil <phil.aldridge@bjss.com>
2023-11-07 10:47:20 +0000

    Updated deployment variables

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <div className='h-72  w-full text-center justify-center flex items-center text-6xl font-bold tracking-wide'>
         <span className='bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 hover:from-pink-800 hover:to-violet-600 transition duration-500'>Map of Mathematics</span></div>
       
-      <Graph/>
+      <Graph />
       
     </main>
   )
