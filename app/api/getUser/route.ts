488ddb3d6665dbb199f6724d2d26ebd3286e63f2
phil <phil.aldridge@bjss.com>
2023-11-07 10:39:22 +0000

    deleted getUser route

--- app/api/getUser/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest } from "next/server"
-
-export function GET(req: NextRequest) {
-    const token = req.cookies.get('token')?.value;
-    if(!token) return;
-    return Response.json({message:"success"})
-}
